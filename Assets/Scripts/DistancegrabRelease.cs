0ee22457c9b250352dae51a6f27d9a469847366b
Sanghwa Song <cnrrn421@gmail.com>
2024-07-29 18:13:59 +0900

    [Feat] 양손 Physics Hand 적용 중

--- Assets/Scripts/DistancegrabRelease.cs
+++ Assets/Scripts/DistancegrabRelease.cs
@@ -5,65 +5,58 @@ using Oculus.Interaction.HandGrab;
 public class DistanceGrabRelease : MonoBehaviour
 {
     [Tooltip("Reference to the DistanceGrabInteractor component.")]
-    [SerializeField]
-    private DistanceGrabInteractor distanceGrabInteractor;
 
-    [SerializeField] 
-    private HandGrabInteractor handGrabInteractor;
+    [SerializeField] private DistanceGrabInteractor leftDistanceGrabInteractor;
+    [SerializeField] private DistanceGrabInteractor rightDistanceGrabInteractor;
+    
+
+    [SerializeField] private HandGrabInteractor leftHandGrabInteractor;
+    [SerializeField] private HandGrabInteractor rightHandGrabInteractor;
 
     [Tooltip("The target transform where the object will be released.")]
-    [SerializeField]
-    private Transform targetTransform;
+    
+    [SerializeField] Transform leftHandAnchor;
+    [SerializeField] Transform rightHandAnchor;
 
     [Tooltip("Distance threshold to automatically release the grab.")]
     [SerializeField]
     private float releaseDistanceThreshold = 0.1f;
-
-    [SerializeField]
-    private bool allowForceGrab = true;
-
-    private void Start()
-    {
-        
-    }
     
     private void Update()
     {
-        if (distanceGrabInteractor == null || targetTransform == null)
+        if (leftDistanceGrabInteractor.HasSelectedInteractable)
         {
-            Debug.LogError("DistanceGrabInteractor or TargetTransform reference is not set.");
-            return; // 필요한 레퍼런스가 설정되지 않은 경우 업데이트 중단
-        }
-
-        var currentInteractable = distanceGrabInteractor.SelectedInteractable;
-
-        if (currentInteractable != null)
-        {
-            float distanceToTarget = Vector3.Distance(currentInteractable.transform.position, targetTransform.position);
-            Debug.Log($"Distance to target: {distanceToTarget}");
-
+            var selectedInteractable = leftDistanceGrabInteractor.SelectedInteractable;
+            float distanceToTarget = Vector3.Distance(selectedInteractable.transform.position, leftHandAnchor.position);
             if (distanceToTarget <= releaseDistanceThreshold)
             {
                 Debug.Log("Within release distance threshold. Releasing grab.");
-                ReleaseGrab();
-            }
-        }
-    }
-
-    private void ReleaseGrab()
-    {
-        var selectedInteractable = distanceGrabInteractor.SelectedInteractable;
-        if (selectedInteractable != null)
-        {
-            distanceGrabInteractor.Unselect(); // 선택 해제
-            Debug.Log("Grab released.");
-
-            if (allowForceGrab)
-            {
+                leftDistanceGrabInteractor.Unselect();
+                Debug.Log("Distance Grab released.");
+                
                 var newInteractable = selectedInteractable.gameObject.GetComponent<HandGrabInteractable>();
-                handGrabInteractor.ForceSelect(newInteractable, true);
+                leftHandGrabInteractor.ForceSelect(newInteractable, true);
+                Debug.Log("Hand Grab Forced");
             }
         }
+        
+        if (rightDistanceGrabInteractor.HasSelectedInteractable)
+        {
+            var selectedInteractable = rightDistanceGrabInteractor.SelectedInteractable;
+            float distanceToTarget = Vector3.Distance(selectedInteractable.transform.position, rightHandAnchor.position);
+            if (distanceToTarget <= releaseDistanceThreshold)
+            {
+                Debug.Log("Within release distance threshold. Releasing grab.");
+                rightDistanceGrabInteractor.Unselect();
+                Debug.Log("Distance Grab released.");
+                
+                var newInteractable = selectedInteractable.gameObject.GetComponent<HandGrabInteractable>();
+                rightHandGrabInteractor.ForceSelect(newInteractable, true);
+                Debug.Log("Hand Grab Forced");
+            }
+        }
+        
+        
     }
     
 }
