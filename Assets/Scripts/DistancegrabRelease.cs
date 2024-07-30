8b6703cec6eaa9dd503d4a597bc43055dac2a9d3
Sanghwa Song <cnrrn421@gmail.com>
2024-07-30 20:09:26 +0900

    [Fix] 양손 physics hand 적용

--- Assets/Scripts/DistancegrabRelease.cs
+++ Assets/Scripts/DistancegrabRelease.cs
@@ -5,87 +5,39 @@ using Oculus.Interaction.HandGrab;
 
 public class DistanceGrabRelease : MonoBehaviour
 {
-    [Tooltip("Reference to the DistanceGrabInteractor component.")]
-
-    [SerializeField] private DistanceGrabInteractor leftDistanceGrabInteractor;
-    [SerializeField] private DistanceGrabInteractor rightDistanceGrabInteractor;
+    [Tooltip("Find Interactors")]
+    [SerializeField]
+    private Transform OVRControllerDrivenHands;
+    private HandGrabInteractor[] newInteractors;
     
-
-    [SerializeField] private HandGrabInteractor leftHandGrabInteractor;
-    [SerializeField] private HandGrabInteractor rightHandGrabInteractor;
-
-    [Tooltip("The target transform where the object will be released.")]
+    [Tooltip("Select which hand to use for Force Select.")]
+    [SerializeField]
+    private HandGrabInteractable handGrabInteractable;
+    private Handedness interactableHandedness;
     
-    [SerializeField] Transform leftHandAnchor;
-    [SerializeField] Transform rightHandAnchor;
-
     [Tooltip("Distance threshold to automatically release the grab.")]
     [SerializeField]
     private float releaseDistanceThreshold = 0.1f;
 
-    [SerializeField] 
-    private Transform OVRControllerDrivenHands;
-
-    [SerializeField] private HandGrabInteractable interactable;
-    
-    private void Update()
+    private void Start()
     {
-        return;
-        
-        if (leftDistanceGrabInteractor.HasSelectedInteractable)
-        {
-            var selectedInteractable = leftDistanceGrabInteractor.SelectedInteractable;
-            float distanceToTarget = Vector3.Distance(selectedInteractable.transform.position, leftHandAnchor.position);
-            if (distanceToTarget <= releaseDistanceThreshold)
-            {
-                Debug.Log("Within release distance threshold. Releasing grab.");
-                leftDistanceGrabInteractor.Unselect();
-                Debug.Log("Distance Grab released.");
-                
-                var newInteractable = selectedInteractable.gameObject.GetComponent<HandGrabInteractable>();
-                leftHandGrabInteractor.ForceSelect(newInteractable, true);
-                Debug.Log("Hand Grab Forced");
-            }
-        }
-        
-        if (rightDistanceGrabInteractor.HasSelectedInteractable)
-        {
-            var selectedInteractable = rightDistanceGrabInteractor.SelectedInteractable;
-            float distanceToTarget = Vector3.Distance(selectedInteractable.transform.position, rightHandAnchor.position);
-            if (distanceToTarget <= releaseDistanceThreshold)
-            {
-                Debug.Log("Within release distance threshold. Releasing grab.");
-                rightDistanceGrabInteractor.Unselect();
-                Debug.Log("Distance Grab released.");
-                
-                var newInteractable = selectedInteractable.gameObject.GetComponent<HandGrabInteractable>();
-                rightHandGrabInteractor.ForceSelect(newInteractable, true);
-                Debug.Log("Hand Grab Forced");
-            }
-        }
-        
-        
+        newInteractors = OVRControllerDrivenHands.GetComponentsInChildren<HandGrabInteractor>(true);
+        if (handGrabInteractable == null) handGrabInteractable = GetComponent<HandGrabInteractable>();
+        interactableHandedness = handGrabInteractable.HandGrabPoses[0].HandPose.Handedness;
     }
 
     public void ReleaseDistanceGrab(PointerEvent pointerEvent)
     {
         DistanceGrabInteractor interactor = pointerEvent.Data as DistanceGrabInteractor;
-        if (interactor == null) return;
+        if (interactor == null || handGrabInteractable == null) return;
 
-        Handedness handedness = interactor.GetComponent<ControllerRef>().Handedness;
-        
-        interactor.Unselect();
-        ForceHandGrab(handedness);
+        Handedness interactorHandedness = interactor.GetComponent<ControllerRef>().Handedness;
+
+        if (interactorHandedness == interactableHandedness)
+        {
+            interactor.Unselect();
+            HandGrabInteractor newInteractor = newInteractors[(int) interactorHandedness];
+            newInteractor.ForceSelect(handGrabInteractable, true);
+        }
     }
-
-    public void ForceHandGrab(Handedness handedness)
-    {
-        if (interactable == null) return;
-
-        HandGrabInteractor[] interactors = OVRControllerDrivenHands.GetComponentsInChildren<HandGrabInteractor>(true); 
-        HandGrabInteractor interactor = interactors[(int) handedness];
-        
-        interactor.ForceSelect(interactable, true);
-    }
-    
 }
